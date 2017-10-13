1848fee5695f90c0d94b29ce53257567c31477dd
驱蚊小将 <13852291705@163.com>
2017-10-13 17:29:59 +0800

    管理资源管理输出
    管理资源管理输出

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
 import _ from 'lodash'
-import './style.css'
+// import './style.css'
+// import Icon from './tan.jpg'
+// import Data from './data.xml'
+import printMe from './print.js'
+
 function component(){
     var element = document.createElement("div");
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+    var btn = document.createElement("button");
+    element.innerHTML = _.join(['Hello', 'webpack',"yes"], ' ');
+    btn.innerHTML = 'Click me and check the console! ';
+    btn.onclick = printMe;
+    element.appendChild(btn);
+    //element.classList.add('hello');
+
+    //将图像添加到我们现有的div
+    //var myIcon = new Image();
+    //myIcon.src = Icon;
+
+   // element.appendChild(myIcon);
+
+    //console.log(Data);
     return element;
 }
 document.body.appendChild(component());
